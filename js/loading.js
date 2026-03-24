fcd43aa192ea408e123847ac38f971c5517e522d
kodamaaaaaa <yzhangze14@gmail.com>
2026-03-24 14:36:06 +0900

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const bar = document.getElementById("bar");
+  const text = document.getElementById("text");
+  const loader = document.getElementById("loader");
+
+  let progress = 0;
+  let t = 0;
+  const arr = ["Loading.", "Loading..", "Loading..."];
+
+  const txtTimer = setInterval(() => {
+    text.textContent = arr[t % 3];
+    t++;
+  }, 400);
+
+  const timer = setInterval(() => {
+    progress += 2;
+    bar.style.width = progress + "%";
+
+    if (progress >= 100) {
+      clearInterval(timer);
+      clearInterval(txtTimer);
+
+      loader.style.opacity = "0";
+      loader.style.transition = "0.8s";
+
+      setTimeout(() => {
+        window.location.href = "main.html";
+      }, 800);
+    }
+  }, 60);
+});
